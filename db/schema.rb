53dc615e33676bbd4c7aa67aa9762a1762aaec72
Davis JR <davisjr2000@hotmail.com>
2018-11-03 14:14:47 -0300

    Made the migration with postgre

--- db/schema.rb
+++ db/schema.rb
@@ -12,9 +12,35 @@
 
 ActiveRecord::Schema.define(version: 2018_11_03_130629) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "cocktails", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "photo"
+  end
+
+  create_table "doses", force: :cascade do |t|
+    t.string "description"
+    t.bigint "cocktail_id"
+    t.bigint "ingredient_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["cocktail_id"], name: "index_doses_on_cocktail_id"
+    t.index ["ingredient_id"], name: "index_doses_on_ingredient_id"
+  end
+
+  create_table "ingredients", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "posts", force: :cascade do |t|
     t.text "content"
-    t.integer "user_id"
+    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_posts_on_user_id"
@@ -29,4 +55,7 @@ ActiveRecord::Schema.define(version: 2018_11_03_130629) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "doses", "cocktails"
+  add_foreign_key "doses", "ingredients"
+  add_foreign_key "posts", "users"
 end
